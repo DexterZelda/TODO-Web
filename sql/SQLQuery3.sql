5d47481ef0a9d790f19b8417110237811a147f3a
David CHANTOISEAU <gogs@fake.local>
2020-02-21 09:44:36 +0100

    TODO maj Version 2.0 stable

--- sql/SQLQuery3.sql
+++ sql/SQLQuery3.sql
@@ -1,5 +1,5 @@
 CREATE TABLE utilisateurs (
 id INT not null identity (1,1),
-nom VARCHAR(50) not null,
-mdp VARCHAR(50) not null
+nom NVARCHAR(50) not null,
+mdp NVARCHAR(50) not null
 );
--- sql/SQLQuery7.sql
+++ sql/SQLQuery7.sql
@@ -1,5 +1,5 @@
 CREATE TABLE toDo (
 id INT not null identity (1,1),
-libelle VARCHAR(50) not null,
+libelle NVARCHAR(50) not null,
 idUser INT not null
 );
