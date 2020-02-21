6cf513f63e820280098e88f6017b2d9cfdd6390a
David CHANTOISEAU <gogs@fake.local>
2020-02-21 11:10:17 +0100

    Version 3.0 stable

--- sql/SQLQuery3.sql
+++ sql/SQLQuery3.sql
@@ -3,3 +3,9 @@ id INT not null identity (1,1),
 nom NVARCHAR(50) not null,
 mdp NVARCHAR(50) not null
 );
+
+/** pour le PB d'enregistrement des caractères spéciaux une solution partielle ? :
+ * https://github.com/microsoft/sql-server-samples/blob/master/samples/features/unicode/CheckStorageReq_CurrDB.sql
+ * https://docs.microsoft.com/fr-fr/sql/relational-databases/collations/collation-and-unicode-support?view=sql-server-ver15#utf8
+ * https://docs.microsoft.com/fr-fr/sql/relational-databases/collations/collation-and-unicode-support?view=sql-server-ver15
+ */
